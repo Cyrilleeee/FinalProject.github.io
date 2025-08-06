f6185555d4cf24fd8f48361a9cdaba1376e15bc6
Cyrille Candelaria <cyrillecandelaria3@gmail.com>
2025-08-06 13:12:06 -0400

    Update script.js

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,1 +1,19 @@
-
+AOS.init({
+  duration: 1200,
+  easing: 'ease-in-out',
+});
+window.onscroll = function() {
+const btn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  }
+  else {
+    btn.style.display = "none";
+  }
+  };
+  document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+  });
